e89ab6c3c1ad89adffc223555cb63d59bce81b04
Dave Richardson <110895199+dave-m-r@users.noreply.github.com>
2022-12-10 09:23:10 +0000

    day 10

---
+++ 2022/10/script.py
@@ -0,0 +1,49 @@
+import sys
+
+x = 1
+cycle = 1
+
+report = {}
+
+grid = [["."] * 40 for i in range(7)]
+
+def show_grid():
+    for r in grid[0:6]:
+        print("".join(r))
+
+
+def maybe_report(line):
+    #print(f"c:{cycle} x: {x} {line}")
+    if cycle in [20, 60, 100, 140, 180, 220]:
+        report[cycle] = x
+    
+    sprite = [x-1,x,x+1]
+    if (cycle - 1) % 40 in sprite:
+        row = (cycle-1) // 40
+        col = (cycle-1)% 40
+        grid[row][col] = '#'
+    #show_grid()
+
+for line in sys.stdin.read().splitlines():
+    cycle += 1
+    maybe_report(line)
+    
+    match line[0:4]:
+        case 'noop':
+            ...
+        case 'addx':
+            x += int(line[5:])
+            cycle += 1
+            maybe_report(line)
+
+        case _:
+            raise Exception(line)
+
+
+print(report)
+print(f"{x} after {cycle} cycles")
+
+ss = sum([k*v for k,v in report.items()])
+print(ss)
+
+show_grid()
