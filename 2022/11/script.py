08e935f613aa2edd8d16184c5604ec579aa7ef5c
Dave Richardson <110895199+dave-m-r@users.noreply.github.com>
2022-12-11 12:15:18 +0000

    day 11 final
    back out an error - using callables for the op broke it

--- 2022/11/script.py
+++ 2022/11/script.py
@@ -2,8 +2,8 @@ import sys
 from dataclasses import dataclass,field
 from typing import Callable
 
-part_1 = True
-logging = False
+
+logging = True
 
 def log(txt_call):
     if(logging):
@@ -17,14 +17,14 @@ class Monkey:
     test_true_target: int = -1
     test_false_target: int = -1
     inspect_count: int = 0
-
-    opc: Callable[[int],int] = lambda x: x
+    op: str = ''
+    opv: int = None
 
     def __repr__(self) -> str:
         return "\n".join([
             f"Monkey {self.id}:",
             f"\tItems: {self.items}",
-            f"\tOperation: {self.opc}",
+            f"\tOperation: {self.op} {self.opv}",
             f"\tTest: divisible by {self.test_divi}",
             f"\t\tIf true: throw to monkey {self.test_true_target}",
             f"\t\tIf false: throw to monkey {self.test_false_target}",
@@ -36,6 +36,7 @@ class Monkey:
     def inspect_item(self,item,monkey,bored_fn):
         self.inspect_count += 1
         log(lambda: f"  Monkey inspects an item with a worry level of {item}")
+        
         new_worry_level = self._do_op(item)
         log(lambda: f"     Worry level goes from {item} to {new_worry_level}")
 
@@ -44,12 +45,18 @@ class Monkey:
         
         is_divi = self._do_test(bored_now)
         log(lambda: f"     Current worry level dividible by {self.test_divi}: {is_divi}")
+        
         target = self.test_true_target if is_divi else self.test_false_target
         log(lambda: f"     Item with worry level {bored_now} is thrown to monkey {target}")
+        
         monkeys[target].catch(bored_now)
 
     def _do_op(self,old):
-        return self.opc(old)
+        v = old if self.opv is None else self.opv
+        match self.op:
+            case '*': return old*v
+            case '+': return old+v 
+            case _: raise Exception(self)
 
     def _do_test(self,wl):
         return (wl % self.test_divi) == 0
@@ -72,17 +79,8 @@ def parse():
             m.items = list(map(int,line.removeprefix("  Starting items: ").split(", ")))
         elif line.startswith("  Operation"):
             (_,op,b) = line.removeprefix("  Operation: new = ").split(" ")
-            
-            if (b=='old' and op =='*'):
-                f = lambda x: x*x
-            else:
-                b = int(b)
-                match op:
-                    case '*': f = lambda x: x*b
-                    case '+': f = lambda x: x+b
-                    case '-': f = lambda x: x-b
-                    case _: raise Exception(line)
-            m.opc = f
+            m.op = op
+            m.opv = None if b=='old' else int(b)
         elif line.startswith("  Test"):
             m.test_divi = int(line.removeprefix("  Test: divisible by "))
         elif line.startswith("    If true"):
@@ -100,20 +98,19 @@ monkeys = parse()
 
 for m in monkeys: log(lambda: str(m))
 
-if (part_1):
-    bored_fn = lambda x: x//3
-else:
+bored_fn = lambda x: x//3
+if not part_1:
     mod = 1
     for m in monkeys:
         mod *= m.test_divi
     bored_fn = lambda x: x % mod
 
-for round_idx in range(rounds):
+for _ in range(rounds):
     for m in monkeys:
         m.take_turn(monkeys,bored_fn)
 
 for m in monkeys:
-    print(f"Monkey {m.id}: {m.inspect_count}")
+    log(lambda: f"Monkey {m.id}: {m.inspect_count}")
 
 ics = sorted([m.inspect_count for m in monkeys],reverse=True)
 print(ics[0]*ics[1])
