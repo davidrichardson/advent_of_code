4f9c0db647bfb8d12cad6953c5d7b375a756ee2d
Dave Richardson <110895199+dave-m-r@users.noreply.github.com>
2022-12-15 20:30:47 +0000

    day 15 complete

--- 2022/15/script.py
+++ 2022/15/script.py
@@ -1,65 +1,87 @@
 import re
+from dataclasses import dataclass
 from collections import Counter
-
-def min_max(items):
-    return [min(items),max(items)]
-
-def bounds(grid):
-    xlim = min_max([x for x,_ in grid.keys()])
-    ylim = min_max([y for _,y in grid.keys()])
-    return (xlim,ylim)
-
-def print_grid(grid):
-    if len(grid) == 0: return "<empty>"
-    (xlim,ylim) = bounds(grid)
-    for y in range(ylim[0],ylim[1]+1):
-        row = [ grid[(x,y)] if (x,y) in grid else '.' for x in range(xlim[0],xlim[1]+1)]
-        print("".join(row))
-
-def sensor_point(grid,x,y):
-    grid[(x,y)] = 'S'
-
-def beacon_point(grid,x,y):
-    grid[(x,y)] = 'B'
-
-def scanned_point(grid,x,y):
-    grid[(x,y)] = '#'
-
+from functools import reduce
 def read_input():
     return [list(map(int,re.findall(r"-?\d+",l))) for l in open(0).read().splitlines()]
         
-def phase_1():
-    rows_of_interest = { r:{} for r in [10,2000000] }
+                
+@dataclass
+class Sensor():
+    x: int
+    y: int
+    radius: int
+    min_y: int
+    max_y: int
 
-    for sx,sy,bx,by in read_input():
+    def senses_row(self,y):
+        return y >= self.min_y and y <= self.max_y
+
+    def sense_xrange_at_y(self,y):
+        dy = abs(self.y - y)
+        dx = abs(dy - self.radius)                 
+        return [self.x - dx, self.x+dx]
+
+    def __repr__(self):
+        return f"Sensor: ({self.x},{self.y}) r:{self.radius} y:{self.min_y}<->{self.max_y})"
+
+def to_sensor(sx,sy,bx,by):
+    r = abs(sx - bx) + abs ( sy - by)
+    return Sensor(sx,sy,r,sy-r,sy+r)
+
+def relevant_sensors(y,sensors):
+    return [s for s in sensors if s.senses_row(y)]    
+
+def merge_ranges(ranges):
+    if (len(ranges)<2): return ranges
+
+    accumulator = [ranges[0]]
+    #relies on sorted ranges
+    def reducer(rg):
+        head = accumulator[-1]
+        if rg[0]<=head[1]+1:
+            head[1] = max(head[1],rg[1])
+        else:
+            accumulator.append(rg)
+
+    for r in ranges[1:]: reducer(r)
+
+    return accumulator
+
+
+def phase_1():
+    sensors = []
+
+    sensors = [to_sensor(sx,sy,bx,by) for sx,sy,bx,by in read_input()]
+ 
+    for y in [10,2000000]:
+        rs = relevant_sensors(y,sensors)
+        
+        ranges = sorted([s.sense_xrange_at_y(y) for s in rs],key=lambda r:r[0])    
+        ranges = merge_ranges(ranges)
+        c = sum(map(lambda r: r[1]-r[0],ranges))
+        print(f"{y} {c}")
+
+
+def phase_2():
+    sensors = []
+
+    sensors = [to_sensor(sx,sy,bx,by) for sx,sy,bx,by in read_input()]
+    
+    limit = 4000000 # should complete within this, but may as well use it
+
+    for y in range(limit+1): 
+        rs = relevant_sensors(y,sensors)
+        
+        sensor_ranges_in_row = sorted([s.sense_xrange_at_y(y) for s in rs],key=lambda r:r[0])    
+        deduped_ranges = merge_ranges(sensor_ranges_in_row)
+ 
+        if len(deduped_ranges)>1:
+            #report the gap
+            x = deduped_ranges[0][1]+1
+            print((x*4000000)+y)
+            return (x,y)
         
 
-        manhatten_distance = abs(sx - bx) + abs ( sy - by)
-
-        for y,row in rows_of_interest.items():
-            if y == sy: sensor_point(row,sx,sy)
-            if y == by: beacon_point(row,bx,by)
-
-            if y >= sy - manhatten_distance and y <= sy+manhatten_distance:
-                dy = abs(sy - y)
-                dx = abs(dy - manhatten_distance)                 
-                
-                for x in range(sx - dx, sx + dx+1):
-                    if (x,y) not in row:
-                        scanned_point(row,x,y)
-
-    if (rows_of_interest[2000000]=={}):
-        grid = {}
-        for row in rows_of_interest.values():
-            for (x,y),c in row.items():
-                grid[(x,y)] = c
-        print_grid(grid)
-
-    for y,row in rows_of_interest.items():
-        counter = Counter(row.values())
-        count = counter['#']+counter['S']
-        print(f"{y} {count}")
-                
-
-phase_1()
+print(phase_2())
 
