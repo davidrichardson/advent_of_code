6653ddfcefcb97a7680123c5aa4e8579585b3c8d
Dave Richardson <110895199+dave-m-r@users.noreply.github.com>
2022-12-16 09:11:56 +0000

    Day 16 - started

--- 2022/16/script.py
+++ 2022/16/script.py
@@ -1,1 +1,24 @@
-for l in open(0).read().splitlines()
+from dataclasses import dataclass
+
+@dataclass
+class Valve():
+    id: str
+    flow: int
+    edges: list[str]
+
+    def __repr__(self):
+        return f"Valve(id:{self.id} flow:{self.flow} edges:{','.join(self.edges)})"
+
+def load_valves():
+    lookup = {}
+    for l in open(0).read().splitlines():
+        tokens = l.split(" ")
+        id = tokens[1]
+        flow = int(tokens[4].removeprefix("rate=").removesuffix(";"))
+        edges = [id.removesuffix(",") for id in tokens[9:]]
+        lookup[id] = Valve(id,flow,edges)
+
+    return lookup
+
+valves_by_id = load_valves()
+print(valves_by_id)
