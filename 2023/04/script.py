b94c5473b1816dec6722479aeef205a88e99722a
David Richardson <davidrichardson@users.noreply.github.com>
2023-12-04 18:07:12 +0000

    tidy up day 4

--- 2023/04/script.py
+++ 2023/04/script.py
@@ -3,32 +3,27 @@ import re
 from collections import defaultdict, Counter
 
 
-def line_count(line: str) -> int:
+def win_count_for_line(line: str) -> int:
     idx = line.find(':')
     
+    counter = Counter((m.group() for m in re.compile('\d+').finditer(line[idx:])))
     
-    
-    numbers_pattern = re.compile('\d+')
-    counter = Counter((m.group() for m in numbers_pattern.finditer(line[idx:])))
-    count = sum(1 for c in counter.values() if c > 1)
-    return count
+    return sum(1 for c in counter.values() if c > 1)
 
-    
-def score_count(count: int) -> int:
+def score_for_count(count: int) -> int:
     return 2**(count - 1)
-
 
 total_score = 0
 total_count = 0
 counter = defaultdict(lambda: 1)
 
 for idx,line in enumerate(sys.stdin):
-    count = line_count(line)
+    count = win_count_for_line(line)
 
     total_count = total_count + counter[idx]
 
     if count > 0:
-        score = score_count(count)
+        score = score_for_count(count)
         total_score = total_score + score
 
         for incr in range(1,count+1):
