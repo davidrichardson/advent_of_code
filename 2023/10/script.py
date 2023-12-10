63d5330f511cefbcafb3e4cf7a8917c0b5ad84f6
David Richardson <davidrichardson@users.noreply.github.com>
2023-12-10 16:46:48 +0000

    day 10 part 2

--- 2023/10/script.py
+++ 2023/10/script.py
@@ -1,6 +1,7 @@
 import sys
-from collections import defaultdict
-from itertools import chain
+from collections import defaultdict,deque
+
+
 N = 0,-1#top of map, low y
 S = 0,1 #bottom of map, high y
 E = 1,0 #right of map, high x
@@ -14,11 +15,18 @@ pipe_ref = {
     '7': (S,W),
     'F': (S,E)
 }
-def parse() -> tuple[dict[tuple[int,int],tuple[tuple[int,int],tuple[int,int]]],tuple[int,int]]:
+
+def read():
+    lines = []
+    for l in sys.stdin:
+        lines.append(l.rstrip())
+    return lines
+
+def parse(lines: list[str]) -> tuple[dict[tuple[int,int],tuple[tuple[int,int],tuple[int,int]]],tuple[int,int]]:
     layout = defaultdict(lambda: '.')
     start = None
-    for idy,line in enumerate(sys.stdin):
-        for idx,c in enumerate(line.rstrip()):
+    for idy,line in enumerate(lines):
+        for idx,c in enumerate(line):
             if c != '.':
                 layout[(idx,idy)] = c
             if c == 'S':
@@ -38,6 +46,7 @@ def next_step(layout,steps):
 
 def part_one(layout,start):
     possible_start_steps = []
+    possible_start_moves = []
 
     #start pipe type is unclear
     for d in (N,S,E,W):
@@ -45,7 +54,11 @@ def part_one(layout,start):
         pipe_at_pos = layout[pos]
         rd = tuple([x * -1 for x in d])
         if pipe_at_pos != '.' and rd in pipe_ref[pipe_at_pos]:
+            possible_start_moves.append(d)
             possible_start_steps.append(move(start,d))
+    for k,v in pipe_ref.items():
+        if all(d in v for d in possible_start_moves):
+            layout[start] = k
 
     steps = 1
     fwd_steps = [start,possible_start_steps[0]]
@@ -57,20 +70,68 @@ def part_one(layout,start):
         next_step(layout,rev_steps)
     return steps,fwd_steps,rev_steps
 
-layout,start = parse()
+def part_two(steps,layout):
+    x_s, y_s = set(),set()
+    steps=set(steps)
+    for p in steps:
+        x_s.add(p[0])
+        y_s.add(p[1])
+    max_x = max(x_s)
+    max_y = max(y_s)
 
-steps,fwd_steps,rev_steps = part_one(layout,start)
-print("part 1",steps)
+    area = 0
 
-max_x,max_y = 0,0
-new_layout = defaultdict(lambda: '.')
-for p in chain(fwd_steps,rev_steps):
-    new_layout[p] = layout[p]
-    max_x = max(max_x,p[0])
-    max_y = max(max_y,p[1])
+    for y in range(max_y+1):
+        norths = 0
+        for x in range(max_x+1):
+            con = layout[(x,y)]
 
-simple_layout = []
-for y in range(0,max_y+1):
-    simple_layout.append( [new_layout[(x,y)] for x in range(0,max_x+1)] )
+            if (x,y) in steps:
+                pipe_directions = pipe_ref[con]
+                if N in pipe_directions:
+                    norths += 1
+                continue
 
-for l in simple_layout: print(''.join(l))
+            if norths % 2 != 0:
+                area = area + 1
+    
+    return area
+
+
+def display_loop(layout,fwd_steps,rev_steps):
+    x_s, y_s = set(),set()
+    max_x,max_y = 0,0
+    new_layout = defaultdict(lambda: ' ')
+    steps = fwd_steps + list(reversed(rev_steps))
+
+    for p in steps:
+        new_layout[p] = layout[p]
+        x_s.add(p[0])
+        y_s.add(p[1])
+
+    min_x,max_x = min(x_s),max(x_s)
+    min_y,max_y = min(y_s),max(y_s)
+
+    simple_layout = []
+    for y in range(min_y,max_y+1):
+        simple_layout.append( [new_layout[(x,y)] for x in range(min_x,max_x+1)] )
+
+    tr = str.maketrans("-|F7LJ.", "─│┌┐└┘ ")
+
+    for l in simple_layout: print(''.join(l).translate(tr))
+
+
+
+lines = read()
+layout,start = parse(lines)
+
+num_steps,fwd_steps,rev_steps = part_one(layout,start)
+area = part_two(fwd_steps+rev_steps,layout)
+display_loop(layout,fwd_steps,rev_steps)
+
+print("part 1",num_steps)
+print("part 2",area)
+
+
+    
+
