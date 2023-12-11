8fd80e123ab92b7f5ff8521137fe2269f026fcd9
David Richardson <davidrichardson@users.noreply.github.com>
2023-12-11 21:06:38 +0000

    day 11 tidying

--- 2023/11/script.py
+++ 2023/11/script.py
@@ -1,30 +1,30 @@
 import sys
 from itertools import combinations
 from collections import defaultdict
-
-def parse():
+ 
+def parse() -> tuple[list[int,int],set[int],set[int]]:
     galaxies = []
-    x_gal_index = set()
-    y_gal_index = set()
+    gal_xs = set()
+    gal_ys = set()
 
     for idy,line in enumerate(sys.stdin):
         for idx,c in enumerate(line.rstrip()):
             if c == '#':
                 galaxies.append((idx,idy))
-                x_gal_index.add(idx)
-                y_gal_index.add(idy)
+                gal_xs.add(idx)
+                gal_ys.add(idy)
     
-    return galaxies,x_gal_index,y_gal_index
+    return galaxies,gal_xs,gal_ys
 
-def build_dist_lookup(dim_gal_indexes,expansion_factor):
-    dim_min = min(dim_gal_indexes)
-    dim_max = max(dim_gal_indexes)
+def build_dist_lookup(gals_in_dim,ef):
+    dim_min = min(gals_in_dim)
+    dim_max = max(gals_in_dim)
 
     dist_lookup = defaultdict(lambda: 1)
 
     for d in range(dim_min,dim_max+1):
-        if d not in dim_gal_indexes:
-            dist_lookup[d] = expansion_factor
+        if d not in gals_in_dim:
+            dist_lookup[d] = ef
 
     return dist_lookup
 
@@ -32,26 +32,25 @@ def ordered(a,b):
     if (a > b): return b,a
     else: return a,b 
 
-def dim_distance(a,b,dist_lookup):
+def dim_dist(a,b,dist_lookup):
     l,h = ordered(a,b)
-    return sum((dist_lookup[i] for i in range(l,h)))
+    distances = (dist_lookup[i] for i in range(l,h))
+    return sum(distances)
 
-def distance_sum(expansion_factor,galaxies,x_gal_index,y_gal_index):
-    x_dist_lookup = build_dist_lookup(x_gal_index,expansion_factor)
-    y_dist_lookup = build_dist_lookup(y_gal_index,expansion_factor)
+def distance_sum(ef,galaxies,gal_xs,gal_ys):
+    xdl = build_dist_lookup(gal_xs,ef)
+    ydl = build_dist_lookup(gal_ys,ef)
 
-    total = 0
-    for a,b in combinations(galaxies,2):
-        x_dist = dim_distance(a[0],b[0],x_dist_lookup)
-        y_dist = dim_distance(a[1],b[1],y_dist_lookup)
+    def pair_distance(a,b): 
+        x_dist = dim_dist(a[0],b[0],xdl)
+        y_dist = dim_dist(a[1],b[1],ydl)
+        return x_dist + y_dist
 
-        total = total + x_dist + y_dist
+    return sum(pair_distance(a,b) for a,b in combinations(galaxies,2))
 
-    return total
+galaxies,gal_xs,gal_ys = parse()
 
-galaxies,x_gal_index,y_gal_index = parse()
-
-p1 = distance_sum(2,galaxies,x_gal_index,y_gal_index)
-p2 = distance_sum(1_000_000,galaxies,x_gal_index,y_gal_index)
+p1 = distance_sum(2,galaxies,gal_xs,gal_ys)
+p2 = distance_sum(1_000_000,galaxies,gal_xs,gal_ys)
 
 print(p1,p2)
