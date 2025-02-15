4db051303c8f4a8857adcd02dcee5e56a0133cf9
David Richardson <davidrichardson@users.noreply.github.com>
2025-02-15 13:22:36 +0000

    day 12 done

--- 2024/12/script.py
+++ 2024/12/script.py
@@ -10,8 +10,30 @@ def parse_input() -> dict[Position,Crop]:
             grid[position] = crop
     return grid
 
+def east(p: Position) -> Position: return (p[0]+1, p[1])
+def west(p: Position) -> Position: return (p[0]-1, p[1])
+def north(p: Position) -> Position: return (p[0], p[1]-1)
+def south(p: Position) -> Position: return (p[0], p[1]+1)
+
+def ne(p: Position) -> Position: return (p[0]+1, p[1]-1)
+def se(p: Position) -> Position: return (p[0]+1, p[1]+1)
+def nw(p: Position) -> Position: return (p[0]-1, p[1]-1)
+def sw(p: Position) -> Position: return (p[0]-1, p[1]+1)
+
 def connected_pos(p: Position) -> list[Position]:
-    return ( (p[0]+1, p[1]), (p[0]-1, p[1]), (p[0], p[1]+1), (p[0], p[1]-1) )
+    return ( east(p), west(p), south(p), north(p) )
+
+def corner_nonsense(p: Position):
+    return [
+        (p,north(p),east(p),ne(p)),
+        (p,north(p),west(p),nw(p)),
+        (p,south(p),east(p),se(p)),
+        (p,south(p),west(p),sw(p))
+    ]
+
+def cornering(grid,p: Position):
+    c = lambda x: grid.get(x)
+    return [list(map(c,t)) for t in corner_nonsense(p)] 
 
 def find_a_field(pos,crop,grid) -> set[Position]:
     field = set([pos])
@@ -32,22 +54,20 @@ def perimeter_of_field(field: set[Position]) -> int:
         perimiter += exposed
     return perimiter
 
-def number_of_sides(field: set[Position]) -> int:
-    sides = set()
-
+def number_of_corners(grid,field: set[Position]) -> int:
+    corners = 0
+    
     for pos in field:
-        for d,candidate in enumerate(connected_pos(pos)):
-            if candidate not in field:
-                if pos[0] == candidate[0]:
-                    x = pos[0]
-                else:
-                    x = pos[1]
-                sides.add((d,x))                 
+        for (target,side1,side2,corner) in cornering(grid,pos):
+            if ((target != side1 and target != side2) or
+               (side1 == target and side2 == target and corner != target)):
+                corners +=1
+      
+    
+    return corners
 
-    return len(sides)
-
-grid = parse_input()
-
+ref_grid = parse_input()
+grid  = ref_grid.copy()
 p1_total_price = 0
 p2_total_price = 0
 
@@ -57,7 +77,7 @@ while grid:
     field = find_a_field(seed_pos,crop,grid)
     area = area_of_field(field)
     perimeter = perimeter_of_field(field)
-    sides = number_of_sides(field)
+    sides = number_of_corners(ref_grid,field)
 
     print(f'p1: Field {crop} has area {area} and perimiter {perimeter}. {area} * {perimeter} = {area*perimeter}')
     print(f'p2: Field {crop} has area {area} and sides {sides}. {area} * {sides} = {area*sides}')
@@ -67,3 +87,4 @@ while grid:
 
 print('p1',p1_total_price)
 print('p2',p2_total_price)
+
